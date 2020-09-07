27284d56624bc5dea793ac5ad2cb1591e11e89b6
Lucas K <lucas.kluber@gmail.com>
2020-09-07 00:49:43 -0300

    - implementado metodo do login com usuario e senha

--- api/views/Authentication.py
+++ api/views/Authentication.py
@@ -1,28 +1,26 @@
 import os
+import secrets
+from dotenv import load_dotenv
 from flask_restful import Resource
-from flask_httpauth import HTTPTokenAuth
+from flask_httpauth import HTTPTokenAuth, HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from api.models.ResponseHandler import ResponseHandler as Handler, APIStatus
+
+load_dotenv()
 
 auth = HTTPTokenAuth(scheme='Bearer')
 
 
+tokens = {
+    secrets.token_hex(16):
+        (os.getenv('YC_USERNAME'), generate_password_hash(os.getenv('YC_PWD')))
+}
+
+
 @auth.verify_token
 def verify_token(token):
-
-    tokens = {
-        os.getenv('YC_TOKEN'): os.getenv('YC_USERNAME'),
-    }
-
     if token in tokens:
         return tokens[token]
-
-
-class Logon(Resource):
-    def post(self):
-        """
-        Return token for specified user:password
-        :return:
-        """
-        pass
 
 
 class Token(Resource):
@@ -34,7 +32,10 @@ class Token(Resource):
         Return tokens
         :return:
         """
-        pass
+        return Handler.exception(
+            status=APIStatus.METHOD_NOT_DEFINED,
+            params=None
+        )
 
     @staticmethod
     def post():
@@ -42,7 +43,10 @@ class Token(Resource):
         Create new token
         :return:
         """
-        pass
+        return Handler.exception(
+            status=APIStatus.METHOD_NOT_DEFINED,
+            params=None
+        )
 
     @staticmethod
     def put():
@@ -50,4 +54,36 @@ class Token(Resource):
         Update current Token
         :return:
         """
-        pass
+        return Handler.exception(
+            status=APIStatus.METHOD_NOT_DEFINED,
+            params=None
+        )
+
+
+login = HTTPBasicAuth()
+
+
+@login.verify_password
+def verify_password(username, password):
+    token = None
+    for item in tokens.items():
+        if username == item[1][0] \
+                and check_password_hash(item[1][1], password):
+            token = item[0]
+            break
+    if token:
+        new_token = secrets.token_hex(16)
+        tokens[new_token] = tokens.pop(token)
+        return new_token
+
+
+class Logon(Resource):
+    decorators = [login.login_required]
+
+    @staticmethod
+    def post():
+        """
+        Return token for specified user:password
+        :return:
+        """
+        return Handler.success(response=login.current_user())
