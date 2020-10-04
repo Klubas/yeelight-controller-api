db8f299136f5eaa487f77d41732a7ec753a81a93
Lucas K <lucas.kluber@gmail.com>
2020-10-04 16:21:10 -0300

    - Created logic to generate fake bulbs in LightBulbs.py (get)

--- api/views/LightBulbs.py
+++ api/views/LightBulbs.py
@@ -1,6 +1,6 @@
 import logging
 import traceback
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from api.models.BulbController import BulbController as Bulbs
 from api.views.Authentication import auth
 from api.models.ResponseHandler import ResponseHandler as Handler, APIStatus
@@ -15,9 +15,25 @@ class LightBulbs(Resource):
         Get list of bulbs metadata (ip, name, model, power state, color)
         :return:
         """
+        parser = reqparse.RequestParser()
+        parser.add_argument('fake_bulbs', type=int, required=False, default=0,
+                            help='Generate n fake bulbs for testing purposes')
+        args = parser.parse_args()
+
         try:
             response = Bulbs.get_bulbs(metadata=True)
+
+            fake_response = []
+            for index in range(0, len(response)):
+                fake_response.append(response[index].copy())
+            for index in range(len(response), len(response) + args.fake_bulbs):
+                fake_response.append(response[0].copy())
+            for index in range(len(response), len(fake_response)):
+                fake_response[index]["id"] = "fake_" + str(index)
+            response = fake_response
+
             return Handler.success(response=response)
+
         except Exception as e:
             return Handler.exception(
                 status=APIStatus.ERROR,
